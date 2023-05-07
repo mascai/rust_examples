1d5839c532af4c19b334fcfcdf33d0f8c7ef4e0b
Alex <msumoskalenko@gmail.com>
2023-05-07 16:46:32 +0300

    Update 19_valid_parentheses.rs

--- 01_array/19_valid_parentheses.rs
+++ 01_array/19_valid_parentheses.rs
@@ -3,7 +3,24 @@ https://leetcode.com/problems/valid-parentheses/description/
 
 */
 
+// Solution1
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for ch in s.chars() {
+            match ch {
+                '(' => stack.push(')'),
+                '{' => stack.push('}'),
+                '[' => stack.push(']'),
+                ')' | '}' | ']' if Some(ch) != stack.pop() => return false,
+                _ => (),
+            }
+        }
+        return stack.is_empty();
+    }
+}
 
+// Solution 2
 impl Solution {
     pub fn check(open_bracket: char, close_bracket: char) -> bool {
         return (open_bracket == '(' && close_bracket == ')') ||
