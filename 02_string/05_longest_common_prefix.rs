188a50ccd531d2f87323515b40d10e0e86fedb32
Alex <msumoskalenko@gmail.com>
2023-05-16 00:19:04 +0300

    Update 05_longest_common_prefix.rs

--- 02_string/05_longest_common_prefix.rs
+++ 02_string/05_longest_common_prefix.rs
@@ -3,6 +3,31 @@ https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/str
 
 */
 
+// Solution1
+impl Solution {
+    pub fn longest_pref<'a>(mut s1: &'a str, mut s2: &'a str) -> &'a str {
+        let mut idx: usize = 0;
+        for (c1, c2) in s1.chars().zip(s2.chars()) {
+            if c1 != c2 {
+                break;
+            }
+            idx += c1.len_utf8();
+        }
+        return &s1[0..idx];
+    }
+
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut pref: &str = &strs[0];
+        for i in 1..strs.len() {
+            pref = Self::longest_pref(&pref, &strs[i]);
+        }
+        return pref.to_string();
+    }
+}
+
+
+// Solution2
+
 impl Solution {
     pub fn get_common_prefix<'a>(mut s1: &'a str, mut s2: &'a str) -> &'a str { 
         let mut idx: usize = 0;
