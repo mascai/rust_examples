13edcbe73653fc6d7adf4d58d7efd6b328ab6068
Alex <msumoskalenko@gmail.com>
2023-08-27 11:53:52 +0300

    Update 13_furthest_Point_From_Origin.rs

--- 02_string/13_furthest_Point_From_Origin.rs
+++ 02_string/13_furthest_Point_From_Origin.rs
@@ -4,6 +4,24 @@ https://leetcode.com/problems/furthest-point-from-origin/
 
 impl Solution {
     pub fn furthest_distance_from_origin(moves: String) -> i32 {
+        let [cntl, cntr, cnt_other] = moves
+            .chars()
+            .fold([0, 0, 0], |mut acc, m| {
+                match m {
+                    'L' => acc[0] += 1,
+                    'R' => acc[1] += 1,
+                    '_' => acc[2] += 1,
+                    _ => {}
+                }
+                acc
+            });
+        return (cntl as i32 - cntr).abs() + cnt_other;
+    }
+}
+
+
+impl Solution {
+    pub fn furthest_distance_from_origin(moves: String) -> i32 {
         let mut cntl = 0;
         let mut cntr = 0;
         for ch in moves.chars() {
